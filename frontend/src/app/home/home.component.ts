c248be7d92a01c78f1dd65e61f2eb65a0d0b8bc3
matheusbatistabulb <mard.correa@gmail.com>
2019-05-30 20:52:17 -0300

    bug encontrado na paginação, necessário arrumar

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -10,13 +10,18 @@ import { HttpClient } from '@angular/common/http';
 export class HomeComponent implements OnInit {
 
   public search: String;
-  private apikey = "/?apikey=34c602f9&s="
+  private apikey = "/?apikey=34c602f9&"
   private videoApiUrl = "http://www.omdbapi.com"
   private apiUrl = "localhost:3000/"
-
+  
+  public newStr: String = "";
   public pages: any = [];
   public list: any = [];
   public movies: any = [];
+
+  public isValid: Boolean = true;
+  public searchType: String = "Movie"
+  public typeParam: String = "type=movie";
 
   constructor(
     private http: HttpClient
@@ -27,43 +32,58 @@ export class HomeComponent implements OnInit {
 
   public onSubmit() {
     let str;
-    let newStr: String = "";
     let format: String = "";
-    let searchParam: String = "";
 
-    if(this.search.includes(' ')){
-      
-      str = this.search.split(' ')
+    if(this.search != "" && this.search != null && typeof this.search != undefined) {
 
-      for(let i = 0; i < str.length; i++){
-        format += str[i] + '+'
+      this.isValid = true;
+      if(this.search.includes(' ')){
+        str = this.search.split(' ')
+  
+        for(let i = 0; i < str.length; i++){
+          format += str[i] + '+'
+        }
+        this.newStr = '&s=' + format.substr(0, format.length - 1)
+        // console.log(this.newStr.substr(0, this.newStr.length - 1))
+      } else {
+        this.newStr = '&s=' + this.search;
       }
-      newStr = format.substr(0, format.length - 1)
-      // console.log(newStr.substr(0, newStr.length - 1))
-      searchParam = newStr;
+ 
+      this.http.get(this.videoApiUrl + this.apikey + this.typeParam + this.newStr).subscribe((movies:any) => {
+        this.list = movies.Search;
+        let size = 0;
+        size = Math.ceil(movies.totalResults / 10);
+        for (let i = 1; i <= size; i++){
+          this.pages.push(i);
+        }
+      });
     } else {
-      searchParam = this.search;
+      this.isValid = false;
+      console.log(this.isValid)
     }
+  }
 
+  public changeSearchType(){
 
-    this.http.get(this.videoApiUrl + this.apikey + searchParam).subscribe((movies:any) => {
-
-      this.list = movies.Search;
-       
-      let size = Math.ceil(movies.totalResults / 10);
-
-      for (let i = 1; i <= size; i++){
-
-        this.pages.push(i);
-      }
-
-    });
-
+    if(this.searchType == "Movie") {
+      this.searchType = "TV Show";
+      this.typeParam = "type=series"
+    } else {
+      this.searchType = "Movie";
+      this.typeParam = "type=movie"
+    }
   }
 
   public onClick(e) {
-
     console.log(e.imdbID);
+  }
 
+  public paginate(e) {
+    let page = "&page=" + e
+
+    this.http.get(this.videoApiUrl + this.apikey + this.typeParam + this.newStr + page).subscribe((movies:any) => {
+
+      this.list = movies.Search;
+    });
   }
 }
